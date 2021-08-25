a1f655aa71a2bd76b132b8903d5e7e456cb885e9
Luan Schons <luanschons2000@gmail.com>
2021-08-25 17:03:00 -0300

    up

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,43 +11,16 @@ $('.carousel-perfect-for-all').owlCarousel({
     paginationSpeed : 400,
 })
 
-/*
-var checkboxes = document.querySelectorAll('input');
 
-for( var i = 0; i < checkboxes.length; i++ ) {
-
-  checkboxes[i].addEventListener("change", function() {
-    updateBugs(this);
-  });
-}
-
-function updateBugs(changedElement) {
-  var checkedCount = document.querySelectorAll('input:checked').length;
-
-  // No bugs, thats impossible!
-  if( checkedCount === 0 ) {
-    turnOnRandomBug(changedElement);
-    if( Math.random() > 0.85 ) {
-      turnOnRandomBug(changedElement);
-    }
-  }
-}
-
-function turnOnRandomBug(excluding) {
-  turnOn = Math.floor(Math.random() * checkboxes.length);
-
-  if( checkboxes[turnOn] === excluding) {
-    turnOn = turnOn + 1;
-    if( turnOn > (checkboxes.length - 1)) {
-      turnOn = 0;
-    }
-  }
-
-  checkboxes[turnOn].checked = true;
-}
-
-setTimeout(function() {
-  turnOnRandomBug(null);
-}, 400)
-
-*/
+$('.clients-carousel').owlCarousel({
+  loop:true,
+  center: true,
+  margin:10,
+  nav:false,
+  items:5,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  singleItem:true,
+  slideSpeed : 300,
+  paginationSpeed : 400,
+})
